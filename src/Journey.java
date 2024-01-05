3f17add92158e6b453d714e0efb013e03bbe1d9a
mmasias@kde.home <manuel@masiasweb.com>
2024-01-05 19:58:47 +0100

    Mejora de  Journey y clase Mother
    Se incluye un archivo .gitignore
    Se optimiza la clase Journey, introduciendo un indicador booleano de progreso y modificando la inicialización de variables para iniciar la secuencia de viaje apropiadamente.
    Se añade la definición de la clase Mother para encapsular el comportamiento de avance diario de la madre.

--- src/Journey.java
+++ src/Journey.java
@@ -1,21 +1,36 @@
-
 public class Journey {
 
     private final double INITIAL_DISTANCE;
     private final double MOTHER_ADVANCE_PER_DAY = 80;
     private double remainingDistance;
     private int days;
+    private boolean onJourney;
+
+    Mother mother;
+    Weather weather;
+    Amedio amedio;
+    Marco marco;
 
     public Journey(int initialDistance) {
-        this.INITIAL_DISTANCE = initialDistance;
-        this.remainingDistance = INITIAL_DISTANCE;
-        this.days = 0;
-        Marco marco = new Marco();
-        Mother mother = new Mother(MOTHER_ADVANCE_PER_DAY);
-        Weather weather = new Weather();
-        Amedio amedio = new Amedio();
+        INITIAL_DISTANCE = initialDistance;
+        onJourney = this.INITIAL_DISTANCE > 0;
+        remainingDistance = INITIAL_DISTANCE;
+        days = 0;
+        mother = new Mother(MOTHER_ADVANCE_PER_DAY);
+        weather = new Weather();
+        amedio = new Amedio();
+        marco = new Marco();
     }
 
     public void start() {
+        while (onJourney) {
+            days++;
+            amedio.update();
+            wheather.update();
+            marco.update(wheather, amedio);
+            remainingDistance = remainingDistance - marco.advance() + mother.advance();
+            onJourney = INITIAL_DISTANCE > 0;
+            displayDaySumary(marco, mother, amedio, wheather);
+        }
     }
 }
